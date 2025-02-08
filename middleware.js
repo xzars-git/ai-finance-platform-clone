ad9eb0ba86475492b2fe1c291b49d912c515583b
Arsenius Purbandono <xzars@upi.edu>
2025-02-09 02:42:39 +0700

    FX: fixing middleware clerk that it's can't to /dashboard after login

--- middleware.js
+++ middleware.js
@@ -1,25 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    "/dashboard(.*)",
-    "/account(.*)",
-    "/transaction(.*)",
-])
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)",
+]);
 
-export default clerkMiddleware(async (auth,req) => {
-    const { userId } = auth()
-    if (!userId && isProtectedRoute(req)) {
-        
-        const { redirectToSignIn } = await auth()
-        return redirectToSignIn()
-    }
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
