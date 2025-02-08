9307a0b8bceca9545a2993379367f911049b7678
Arsenius Purbandono <xzars@upi.edu>
2025-02-08 10:20:27 +0700

    BASE: init component ui shadcn

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>
+  return <Button>
     Arsenius Purbandono
-  </div>;
+  </Button>;
 }
