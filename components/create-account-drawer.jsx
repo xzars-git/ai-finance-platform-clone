65d9ea3808b280642f651d6f4a59779996bee1e1
Arsenius Purbandono <xzars@upi.edu>
2025-02-09 23:22:29 +0700

    SLC: slicing account type

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -12,6 +12,13 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { accountSchema } from "@/app/lib/schema";
 import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 const CreateAccountDrawer = ({ children }) => {
   const [open, setOpen] = useState(false);
@@ -43,19 +50,41 @@ const CreateAccountDrawer = ({ children }) => {
         <DrawerHeader>
           <DrawerTitle>Are you absolutely sure?</DrawerTitle>
         </DrawerHeader>
-        <div>
-          <form>
-            <label htmlFor='name' className='text-sm font-medium'>
-              Account Name
-            </label>
-            <Input
-              id='name'
-              placeholder='e.g., Main Checking'
-              {...register("name")}
-            />
-            {errors.name && (
-              <p className='text-sm text-red-500'>{errors.name.message}</p>
-            )}
+        <div className='px-4 pb-4'>
+          <form className='space-y-4'>
+            <div className='space-y-2'>
+              <label htmlFor='name' className='text-sm font-medium'>
+                Account Name
+              </label>
+              <Input
+                id='name'
+                placeholder='e.g., Main Checking'
+                {...register("name")}
+              />
+              {errors.name && (
+                <p className='text-sm text-red-500'>{errors.name.message}</p>
+              )}
+            </div>
+            <div className='space-y-2'>
+              <label htmlFor='type' className='text-sm font-medium'>
+                Account Type
+              </label>
+              <Select
+                onValueChange={(value) => setValue("type", value)}
+                defaultValue={watch("type")}>
+                <SelectTrigger id='type'>
+                  <SelectValue placeholder='Select Type' />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='CURRENT'>Current</SelectItem>
+                  <SelectItem value='SAVINGS'>Savings</SelectItem>
+                </SelectContent>
+              </Select>
+
+              {errors.type && (
+                <p className='text-sm text-red-500'>{errors.type.message}</p>
+              )}
+            </div>
           </form>
         </div>
       </DrawerContent>
